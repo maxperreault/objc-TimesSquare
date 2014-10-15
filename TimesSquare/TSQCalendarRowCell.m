fb37004277b53a7f57b36c8f9028ff4844e02fe8
Mihaela Miches <mihaela.miches@webresint.com>
2014-10-15 12:57:43 +0100

    Change date selection behaviur
    If endData is selected, the next selection will clear the endDate and
    set the startDate to the selectedDate

--- TimesSquare/TSQCalendarRowCell.m
+++ TimesSquare/TSQCalendarRowCell.m
@@ -196,7 +196,10 @@ static const NSInteger maxValueForRange = 14;
     if (self.calendarView.selectionMode == TSQCalendarSelectionModeDay) {
         self.calendarView.selectedDate = ([self.calendarView.selectedDate isEqual:selectedDate]) ? nil : selectedDate;
     } else {
-        if (self.calendarView.selectedStartDate && ([selectedDate compare:self.calendarView.selectedStartDate] == NSOrderedDescending)) {
+        if (self.calendarView.selectedEndDate) {
+            self.calendarView.selectedStartDate = selectedDate;
+            self.calendarView.selectedEndDate = nil;
+        } else if (self.calendarView.selectedStartDate && ([selectedDate compare:self.calendarView.selectedStartDate] == NSOrderedDescending)) {
             if ([self differenceInDaysBetweenStartDate:self.calendarView.selectedStartDate andEndDate:selectedDate] <= maxValueForRange) {
                 self.calendarView.selectedEndDate = selectedDate;
             }
